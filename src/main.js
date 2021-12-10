5b0301b740afa3a1c055165537ca57e072faa6a7
alimukhtor <james1999007@gmail.com>
2021-12-10 15:05:35 +0100

    added errorhandlers and listendPoints

--- src/main.js
+++ src/main.js
@@ -2,7 +2,8 @@ import express from 'express'
 
 import mediaRouter from "./services/media/index.js"
 import reviewsRouter from "./services/reviews/index.js"
-
+import errorHandler from './middleWares/errorHandlers.js'
+import listEndpoints from 'express-list-endpoints'
 
 
 
@@ -14,7 +15,10 @@ const port = process.env.PORT
 
 server.use('/media', mediaRouter)
 server.use('/reviews', reviewsRouter)
+console.table(listEndpoints(server));
 
+
+server.use(errorHandler)
 
 
 
